32d0c8c2fa1489c5f775b5b6867fc8a036d08c0a
BugbearR <bugbearr@bugbearr.jp>
2023-01-23 01:21:38 +0000

    Upgrade to webpack5 and change yarn to pnpm

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,32 @@ module.exports = {
     host: "0.0.0.0",
     port: 8080,
     //useLocalIp: true,
-    public: "0.0.0.0:8080",
-    contentBase: outputPath,
-    disableHostCheck: true
+    allowedHosts: "all",
+    static: {
+      directory: path.resolve(__dirname, "static"),
+      staticOptions: {},
+      // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+      // Can be:
+      // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+      publicPath: "/",
+      // Can be:
+      // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+      serveIndex: true,
+      // Can be:
+      // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+      watch: true,
+    },
+    client: {
+      // Can be `string`:
+      //
+      // To get protocol/hostname/port from browser
+      // webSocketURL: 'auto://0.0.0.0:0/ws'
+      webSocketURL: {
+        hostname: "0.0.0.0",
+        pathname: "/ws",
+        port: 8080,
+      },
+    },
   },
   plugins: [
     new WriteFileWebpackPlugin(),
